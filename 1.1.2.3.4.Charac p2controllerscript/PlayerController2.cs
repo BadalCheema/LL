fcff8a1fb0256324662516be1b5a8af1c36c2202
BadalCheema <66926551+BadalCheema@users.noreply.github.com>
2020-07-23 18:10:13 +0100

    G10

---
+++ 1.1.2.3.4.Charac p2controllerscript/PlayerController2.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+
+    public float walkSpeed = 8;
+    public float runSpeed = 18;
+    public float gravity = -12;
+
+    public float turnSmoothTime = 0.05f;
+    float turnSmoothVelocity;
+
+    public float speedSmoothTime = 0.1f;
+    float speedSmoothVelocity;
+    float currentSpeed;
+    float velocityY;
+
+    Animator animator;
+    //To make the player move with the camera
+    public Transform cameraT;
+    CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        cameraT = Camera.main.gameObject.transform;
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    //Creating a vector2 for keyboard input
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+
+        if (inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        }
+
+        bool running = Input.GetKey (KeyCode.RightShift);
+        float targetSpeed = (running) ? runSpeed : walkSpeed * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        velocityY += Time.deltaTime * gravity;        
+        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
+        currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
+
+        if (controller.isGrounded)
+        {
+            velocityY = 0;
+        }
+
+        float animationSpeedPercent = ((running) ? currentSpeed/runSpeed : currentSpeed/walkSpeed * .5f);
+        animator.SetFloat("Blend", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+    }
+
+
+}
